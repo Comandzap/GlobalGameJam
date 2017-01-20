2d93d4917a4216f0a8f74789045a21c91c5a8632
LAPTOP-3PAIDASL\larsm <lars.magnus.nyland@gmail.com>
2017-01-20 23:52:58 +0100

    Mic Input

--- GlobalGameJamGame/GlobalGameJam/Assets/Scripts/Audio/AudioInput.cs
+++ GlobalGameJamGame/GlobalGameJam/Assets/Scripts/Audio/AudioInput.cs
@@ -10,9 +10,19 @@ public class AudioInput : MonoBehaviour {
     private bool RealtimeOutput;
     private bool lastValueOfRealtimeOutput;
 
-    AudioSource src;
+    int qSamples = 1024;    // Array size
+    float refValue = 0.1f;
+    float threshold = 0.02f; //Minimum amplitude
+    float rmsValue;     // sound level - RMS
+    float dbValue;      // sound level - dB
+    float pitchValue;   // sound pitch - HZ
 
-    public static FFT Instance;
+    private float[] samples;    // Audio samples
+    private float[] spectrum;   // Audio Spectrum
+    float fSample;
+
+    AudioSource src;
+    
 
     private float lastVolume = 0.0f;
 
@@ -24,6 +34,71 @@ public class AudioInput : MonoBehaviour {
         {
             WorkStart();
             Debug.Log("Work Started!");
+        }
+
+        samples = new float[qSamples];
+        spectrum = new float[qSamples];
+        fSample = AudioSettings.outputSampleRate;
+    }
+
+    void AnalyzeSound()
+    {
+        src.GetOutputData(samples, 0); // Fill array with samples
+        int i;
+        float sum = 0.0f;
+
+        for(i = 0; i < qSamples; i++)
+        {
+            sum += samples[i] * samples[i];
+        }
+
+        rmsValue = Mathf.Sqrt(sum / qSamples);
+        dbValue = 20 * Mathf.Log10(rmsValue / refValue);
+
+        if (dbValue < -160) dbValue = -160;
+
+        src.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+
+        float maxV = 0.0f;
+        int maxN = 0;
+
+        for(i = 0; i < qSamples; i++)
+        {
+            if(spectrum[i] > maxV && spectrum[i] > threshold)
+            {
+                maxV = spectrum[i];
+                maxN = i;
+            }
+        }
+
+        float freqN = maxN;
+
+        if(maxN > 0 && maxN < qSamples - 1)
+        {
+            float dL = spectrum[maxN - 1] / spectrum[maxN];
+            float dR = spectrum[maxN + 1] / spectrum[maxN];
+            freqN += 0.5f * (dR * dR - dL * dL);
+        }
+        pitchValue = freqN * (fSample / 2) / qSamples;  // Convert index to freq
+    }
+
+    void FixedUpdate()
+    {
+        AnalyzeSound();
+        //Debug.Log("RMS: " + rmsValue.ToString("F2") + " (" + dbValue.ToString("F1") + " dB)\n"
+        //         + "Pitch: " + pitchValue.ToString("F0") + " Hz");
+
+
+
+        if(pitchValue < 1000 && pitchValue > 0 && dbValue > -10)
+        {
+            Debug.Log("Jump: " + pitchValue.ToString("F0"));
+        } else if(pitchValue > 1000 && dbValue > -10)
+        {
+            Debug.Log("Attack" + pitchValue.ToString("F0"));
+        } else
+        {
+            Debug.Log("Do Nothing!");
         }
     }
 
@@ -86,9 +161,8 @@ public class AudioInput : MonoBehaviour {
         src.loop = true;
         while(!(Microphone.GetPosition(null) > 0))
         {
-            
             src.Play();
-            src.GetSpectrumData()
+            
         }
     }
 
@@ -96,5 +170,33 @@ public class AudioInput : MonoBehaviour {
     {
         Microphone.End(null);
         Debug.Log("Work Stopped!");
+    }
+
+    private int GetMax(float[] Data)
+    {
+        float max = 0.0f;
+        int max_index = 0;
+        for(int i = 0; i < Data.Length; i++)
+        {
+            if(Data[i] > max)
+            {
+                max = Data[i];
+                max_index = i;
+            }
+        }
+
+        return max_index;
+    }
+
+    private float Average(float[] Data)
+    {
+        float sum = 0.0f;
+
+        for(int i = 0; i < Data.Length; i++)
+        {
+            sum += Data[i];
+        }
+
+        return Mathf.Sqrt(sum);
     }
 }
