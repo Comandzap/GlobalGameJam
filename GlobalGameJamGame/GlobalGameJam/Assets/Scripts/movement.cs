2fd9a1953b3d37146cf3a0c55e51a4893f5b55ec
Comandzap <andy528200@gmail.com>
2017-01-21 17:30:46 +0100

    Aiming UI

--- GlobalGameJamGame/GlobalGameJam/Assets/Scripts/movement.cs
+++ GlobalGameJamGame/GlobalGameJam/Assets/Scripts/movement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class movement : MonoBehaviour {
 
+
+	public Transform playerUI;
 	bool jump;
 	public float jumpPower;
 	Rigidbody body;
@@ -15,6 +17,7 @@ public class movement : MonoBehaviour {
 
     public GameObject missile;
 
+	public float facing;
     
 
 	// Use this for initialization
@@ -37,11 +40,35 @@ public class movement : MonoBehaviour {
 		float horizontalForce = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
 		body.AddForce(Vector3.right * horizontalForce);
 
-        if(Input.GetKeyDown(FireButton))
+		
+
+		if (Input.GetAxis("Horizontal")>0.0f)
+		{
+			facing = 1;
+			fireDir = true;
+		}
+		else if (Input.GetAxis("Horizontal") < 0.0f)
+		{
+			facing = -1;
+			fireDir = false;
+		}
+
+		playerUI.transform.position = new Vector2(transform.position.x+ facing, transform.position.y);
+		playerUI.transform.localScale = new Vector3(facing, 1,1);
+
+
+		if (Input.GetButtonDown(FireButton))
         {
-            AudioInput audioClass = this.GetComponent<AudioInput>();
-            (Instantiate(missile, transform.position, Quaternion.identity) as GameObject).GetComponent<missile>().direction(fireDir, audioClass.GetDirectionVector(), audioClass.GetColorTemp());
+			playerUI.gameObject.SetActive(true);
         }
+		if(Input.GetButtonUp(FireButton))
+		{
+			playerUI.gameObject.SetActive(false);
+			AudioInput audioClass = this.GetComponent<AudioInput>();
+            (Instantiate(missile, playerUI.transform.position, Quaternion.identity) as GameObject).GetComponent<missile>().direction(fireDir, audioClass.GetDirectionVector(), audioClass.GetColorTemp());
+		}
+
+		
 	}
 
 
