0525bf66e7cb95dc60801ed0dffef5f8860899a8
Comandzap <andy528200@gmail.com>
2017-01-21 01:10:47 +0100

    Bullets

---
+++ GlobalGameJamGame/GlobalGameJam/Assets/Scripts/missile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class missile : MonoBehaviour {
+
+	Rigidbody body;
+
+	// Use this for initialization
+	void Start ()
+	{
+		body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		body.AddForce(Vector3.right * 40);
+	}
+
+
+}
